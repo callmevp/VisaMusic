e8015903733f7b434a0699130c69b37b91b765bd
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-09-14 23:32:07 +0530

    visa

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 By VeezMusicProject
+# Copyright (C) 2021 By VisaMusicProject
 
 # ===========
 # running bot
@@ -12,40 +12,40 @@ import importlib
 from pathlib import Path
 from pyrogram import Client, idle
 from config import Veez 
-from bot.videoplayer import app
-from bot.videoplayer import call_py
+from VisaMusic.videoplayer import app
+from VisaMusic.videoplayer import call_py
 from helpers.loggings import LOG
  
     
-bot = Client(
+VisaMusic = Client(
     ":memory:",
-    Veez.API_ID,
-    Veez.API_HASH,
-    bot_token=Veez.BOT_TOKEN,
-    plugins=dict(root="bot"),
+    Visa.API_ID,
+    Visa.API_HASH,
+    bot_token=Visa.BOT_TOKEN,
+    plugins=dict(root="VisaMusic"),
 )
 
 StartTime = time.time()
 
 loop = asyncio.get_event_loop()
 
-_path = f"bot/*.py"
+_path = f"VisaMusic/*.py"
 files = glob.glob(_path)
 
 def load_plugins(plugin_name):
-    path = Path(f"bot/{plugin_name}.py")
-    name = "bot.{}".format(plugin_name)
+    path = Path(f"VisaMusic/{plugin_name}.py")
+    name = "VisaMusic.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(load)
-    sys.modules[f"bot." + plugin_name] = load
+    sys.modules[f"VisaMusic." + plugin_name] = load
     print("Imported => " + plugin_name)
 
 async def start():
     print('\n')
-    print('------------------ Initalizing VEEZ --------------------')
-    if bot:
-        await bot.start()
+    print('------------------ Initalizing VISA --------------------')
+    if VisaMusic:
+        await VisaMusic.start()
     await app.start()
     await call_py.start()
     print('------------------------ DONE --------------------------')
@@ -55,18 +55,18 @@ async def start():
             path_ = Path(a.name)
             plugin_name = path_.stem
             load_plugins(plugin_name.replace(".py", ""))
-    print('------------------- INITIATED VEEZ ---------------------')
+    print('------------------- INITIATED VISA ---------------------')
     print('     Logged in as User =>> {}'.format((await app.get_me()).first_name))
-    if bot:
-        print('     Logged in to Bots =>> {}'.format((await bot.get_me()).first_name))
+    if VisaMusic:
+        print('     Logged in to Bots =>> {}'.format((await VisaMusic.get_me()).first_name))
     print('--------------------------------------------------------')
     await idle()
 if __name__ == '__main__':
-    is_bot = bool(Veez.BOT_TOKEN)
+    is_VisaMusic = bool(Visa.BOT_TOKEN)
     loop.run_until_complete(start())
 
 
-# bot.start()
+# VisaMusic.start()
 # print("[STATUS]:✅ »» BOT CLIENT STARTED ««")
 # app.start()
 # print("[STATUS]:✅ »» USERBOT CLIENT STARTED ««")
